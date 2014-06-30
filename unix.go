541f257a1d2a30157866e6443e2532468899c653
Vasily Korytov <vasily.korytov@yahoo.com>
2014-06-30 13:37:24 +0300

    Clean-up

--- unix.go
+++ unix.go
@@ -13,8 +13,10 @@ import (
 
 func setlimits(limit *int) {
 	var old, new syscall.Rlimit
-	er := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &old)
-	if er == nil {
+	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &old)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Unable to access Unix limits:", err)
+	} else {
 		new.Cur = uint64(*limit + 6) // Magic. 1-5 does not work, 6 seems OK.
 		new.Max = new.Cur
 		if old.Cur < new.Cur || old.Max < new.Cur {
@@ -23,7 +25,5 @@ func setlimits(limit *int) {
 				fmt.Fprintln(os.Stderr, "Unable to set Unix limits:", err)
 			}
 		}
-	} else {
-		fmt.Fprintln(os.Stderr, "Unable to access Unix limits:", er)
 	}
 }
