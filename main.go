e79228d13ea052c5f392bf1535b3c4543825a941
Vasily Korytov <vasily.korytov@yahoo.com>
2015-01-10 16:09:19 +0200

    cleaner copyright notice

--- main.go
+++ main.go
@@ -28,20 +28,6 @@ Please note that this utility uses GOMAXPROCS environment variable if it's prese
 If not, this defaults to CPU count + 1. */
 package main
 
-/* Copyright 2014 Vasily Korytov
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License. */
-
 import (
 	"flag"
 	"fmt"
