93984b9f7dbd562633d5c8e40bf6331ffdcaafa8
Vasily Korytov <vasily.korytov@yandex.com>
2015-08-21 20:46:54 +0300

    some cosmetics

--- httpstress_test.go
+++ httpstress_test.go
@@ -9,14 +9,14 @@ func TestTest(t *testing.T) {
 	invalid := []string{"ftp://localhost"}                    // Error. Non HTTP/HTTPS URL.
 
 	if _, err := Test(1, 1, invalid); err == nil {
-		t.Errorf("%s is ok (should be an error)", invalid)
+		t.Errorf("%s ok (should be an error)", invalid)
 	}
 
 	if err, _ := Test(1, 1, up); len(err) > 0 {
-		t.Errorf("%s is down (should be up)", up)
+		t.Errorf("%s down (should be up)", up)
 	}
 
 	if err, _ := Test(1, 1, down); len(err) == 0 {
-		t.Errorf("%s is up (should be down)", down)
+		t.Errorf("%s up (should be down)", down)
 	}
 }
